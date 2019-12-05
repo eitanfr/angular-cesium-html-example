30265b416e470e419080374571dd4bc4cb12436e
eitanfr <eitan.fr5@gmail.com>
2019-12-05 10:52:11 +0200

    2 files modified

--- src/app/html-layer/html-example.component.ts
+++ src/app/html-layer/html-example.component.ts
@@ -18,7 +18,7 @@ export class HtmlExampleComponent implements OnInit {
       id: '0',
       show: true,
       name: 'Html 1',
-      position: Cesium.Cartesian3.fromDegrees(-80, 30),
+      position: Cesium.Cartesian3.fromDegrees(-95, 30),
       color: Cesium.Color.RED
     },
   };
@@ -29,7 +29,7 @@ export class HtmlExampleComponent implements OnInit {
       id: '1',
       show: true,
       name: 'Html 2',
-      position: Cesium.Cartesian3.fromDegrees(-80, 45),
+      position: Cesium.Cartesian3.fromDegrees(-95, 45),
       color: Cesium.Color.RED
     }
   };
